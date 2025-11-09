32faa136ce91d77f1788aacf439adbbbfd9f00b7
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-11-09 21:17:50 +0530

    Correction Changes

--- src/components/Others/RejectModal.jsx
+++ src/components/Others/RejectModal.jsx
@@ -31,7 +31,7 @@ function RejectModal({ showRejectModal, closeModal, selectedStudent }) {
             return;
         }
 
-        const payload = { registerNo: student.registerNo, reason: finalReason }
+        const payload = { registerNo: student.registerNo, reason: finalReason, _id: student._id };
 
         try {
 
