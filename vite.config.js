a3bfef78656b5b9e1df26caa0f5e848954caa282
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2026-06-30 21:00:25 +0530

    Correction

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Inspector from 'vite-plugin-react-inspector'
+
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-	],
+    plugins: [
+        react(),
+        tailwindcss(),
+        Inspector(),
+    ],
 })
